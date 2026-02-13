a9cd9c271b84e219c6babac72a0f84b7f77fc4a5
Greg Pazo <gp@Ben-Dover.local>
2026-02-13 08:12:34 -0800

    indubitably 0.1.0

--- Casks/indubitably.rb
+++ Casks/indubitably.rb
@@ -1,9 +1,9 @@
 cask "indubitably" do
-  version "0.1.0-dev.3"
+  version "0.1.0"
   arch arm: "aarch64", intel: "x86_64"
 
-  sha256 arm:   "d2dbe0d0c2c5044b90b81cc7d7c83e07f6a7dd1d54b83bb0718a8ae7f5fae70d",
-         intel: "c03b380a5d58eb4235c480d2b745171da02f6ae813f2924caf5866802a7d0c45"
+  sha256 arm:   "90fb3ff715d4fd5b80b90f2d2c20d3d8c3065834fa773602fe0ef9b8e8ae027a",
+         intel: "fcdfef41dcdd07482cc4a9888cb05433add9838d1ad22f8b7fb717229959f094"
 
   url "https://downloads.indubitably.ai/cli/indubitably/#{version}/indubitably-#{arch}-apple-darwin.tar.gz"
   name "Indubitably CLI"
