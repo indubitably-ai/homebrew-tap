b25262983d405c4a18e4c37b6407f7a8e1d07c8c
Greg Pazo <gp@Ben-Dover.local>
2026-02-21 12:38:18 -0800

    Release indubitably 0.1.11

--- Casks/indubitably.rb
+++ Casks/indubitably.rb
@@ -1,8 +1,8 @@
 cask "indubitably" do
-  version "0.1.10"
+  version "0.1.11"
   arch arm: "aarch64", intel: "x86_64"
 
-  sha256 arm:   "ffb11c0275700f68fdfbd941cb6b91755d5612ae140cffd33163537b75c71cf5",
+  sha256 arm:   "f13b0187314639de8c3412b43021da363df3ada7617a63af6850b0ba612095d9",
          intel: "11aa1654da2d3e9ed9890a9e9bf7daf0ecfb744822e29c16afabaf96baad6215"
 
   url "https://downloads.indubitably.ai/cli/indubitably/#{version}/indubitably-#{arch}-apple-darwin.tar.gz"
