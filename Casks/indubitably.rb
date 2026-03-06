c344150296affd589780500fe633a017e7cb3334
Greg Pazo <gp@Ben-Dover.local>
2026-03-06 10:45:49 -0800

    Update indubitably cask to 0.1.17

--- Casks/indubitably.rb
+++ Casks/indubitably.rb
@@ -1,9 +1,9 @@
 cask "indubitably" do
-  version "0.1.16"
+  version "0.1.17"
   arch arm: "aarch64", intel: "x86_64"
 
-  sha256 arm:   "760f9be4e16acc8857f94332b3cb24a59ac7df4592f1f2a91b3d9e16d5e401e3",
-         intel: "e79bb08aa041d3632774cddc4c8ca4e7a9e814b69d8045517e14b0c40217a416"
+  sha256 arm:   "85c8dddfc7b6b5519928ea6403f0f9a36c4965297ac357191c7220558ddf8cd5",
+         intel: "22d65239909d24321b1f9350cafb8d86e812ba0dbb73fdf2ee8a28b3b06948a1"
 
   url "https://downloads.indubitably.ai/cli/indubitably/#{version}/indubitably-#{arch}-apple-darwin.tar.gz"
   name "Indubitably CLI"
